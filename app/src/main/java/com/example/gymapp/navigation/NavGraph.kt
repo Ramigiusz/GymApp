f63756354eca8d4478a1b245ef6ef21085aa530f
Jan Galicki <ja45ieg@gmail.com>
2025-05-03 12:27:57 +0200

    strona z rutynami aktualizuje sie razem z dodaniem nowej rutyny

--- app/src/main/java/com/example/gymapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/gymapp/navigation/NavGraph.kt
@@ -1,31 +1,49 @@
 package com.example.gymapp.navigation
 
-// navigation/NavGraph.kt
-
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.gymapp.viewmodel.RoutineViewModel                    // <-- dodaj ten import
 import com.example.gymapp.ui.screens.RoutinesScreen
-import com.example.gymapp.ui.screens.SettingsScreen
 import com.example.gymapp.ui.screens.StartScreen
+import com.example.gymapp.ui.screens.SettingsScreen
 import com.example.gymapp.ui.screens.TrainingScreen
 import com.example.gymapp.ui.screens.EditRoutineScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
+    // jawna deklaracja typu generycznego
+    val routinesViewModel: RoutineViewModel = viewModel<RoutineViewModel>()
+
     NavHost(navController, startDestination = "start") {
-        composable("start") { StartScreen(navController) }
-        composable("settings") { SettingsScreen(navController) }
-        composable("routines") { RoutinesScreen(navController) }
+        composable("start") {
+            StartScreen(navController)
+        }
+
+        // tylko jedna trasa "routines", korzystająca z twojego VM
+        composable("routines") {
+            RoutinesScreen(navController, routinesViewModel)
+        }
+
+        composable("settings") {
+            SettingsScreen(navController)
+        }
+
         composable("training_screen/{routineId}") { backStackEntry ->
-            val routineId = backStackEntry.arguments?.getString("routineId")?.toIntOrNull()
+            val routineId = backStackEntry.arguments
+                ?.getString("routineId")
+                ?.toIntOrNull()
             TrainingScreen(navController, routineId)
         }
+
         composable("edit_routine_screen/{routineId}") { backStackEntry ->
-            val routineId = backStackEntry.arguments?.getString("routineId")?.toIntOrNull()
-            EditRoutineScreen(navController, routineId)
+            val routineId = backStackEntry.arguments
+                ?.getString("routineId")
+                ?.toIntOrNull()
+            // tu podajesz ten sam VM
+            EditRoutineScreen(navController, routineId, routinesViewModel)
         }
     }
 }
